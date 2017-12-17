17f8057c51da4fd98a1010f84873e9cb397996c7
Saqib Omer <saqibomer.cs@gmail.com>
2017-12-17 07:34:56 +0500

    Initial Commit

---
+++ Loadmation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loadmation
+//
+//  Created by Saqib Omer on 17/12/2017.
+//  Copyright © 2017 Saqib Omer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
